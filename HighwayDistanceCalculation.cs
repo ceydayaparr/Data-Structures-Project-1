99e109b7c19bfb8bea5242cf9f527e18d035cf93
ceydayaparr <ceydayapar2@gmail.com>
2025-03-10 14:58:07 +0300

    A Data Structures course project featuring highway distance calculations with search algorithms and a perceptron model for exam score prediction.

---
+++ HighwayDistanceCalculation.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Cities
+    {
+        string[] cities;
+        int[][] distances;
+        ArrayList dictionary;
+        Dictionary<string, Dictionary<int, List<string>>> cityDistances; 
+        Dictionary<string, Dictionary<string, int>> cityToCityDistances; 
+
+        public Cities(string[] cities_, int[][] distances_, ArrayList dictionary_, Dictionary<string, Dictionary<int, List<string>>> cityDistances_, Dictionary<string, Dictionary<string, int>> cityToCityDistances_)
+        {
+            cities = cities_;
+            distances = distances_;
+            dictionary = dictionary_;
+            cityDistances = cityDistances_;
+            cityToCityDistances = cityToCityDistances_;
+        }
+
+        public void ProcessMatrix(string[][] matrix)
+        {
+            for (int i = 2; i < 83; i++)
+            {
+                cities[i - 2] = matrix[i][1];
+                distances[i - 2] = new int[81];
+                for (int j = 2; j < 83; j++)
+                {
+                    int num = 0;
+                    if (!string.IsNullOrEmpty(matrix[i][j]))
+                    {
+                        num = int.Parse(matrix[i][j]);
+                    }
+
+                    distances[i - 2][j - 2] = num;
+                    dictionary.Add(new object[] { matrix[i][1], matrix[1][j], num });
+                    if (!cityDistances.ContainsKey(matrix[i][1]))
+                    {
+                        cityDistances[matrix[i][1]] = new Dictionary<int, List<string>>();
+                        cityToCityDistances[matrix[i][1]] = new Dictionary<string, int>();
+                    }
+                    if (!cityDistances[matrix[i][1]].ContainsKey(num))
+                    {
+                        cityDistances[matrix[i][1]][num] = new List<string>();
+                    }
+                    if (!cityToCityDistances[matrix[i][1]].ContainsKey(matrix[1][j]))
+                    {
+                        cityToCityDistances[matrix[i][1]][matrix[1][j]] = num;
+                    }
+                    cityDistances[matrix[i][1]][num].Add(matrix[1][j]);
+                }
+            }
+        }
+
+        public List<string> GetCoveredCities()
+        {
+            List<string> coveredCities = new List<string>();
+            Console.Write("Enter city name: ");
+            string cityName = Console.ReadLine();
+            Console.Write("Enter distance range: ");
+            int distanceRange = int.Parse(Console.ReadLine());
+            if (distanceRange > 0 && cityDistances.ContainsKey(cityName))
+            {
+                foreach (int distance in cityDistances[cityName].Keys)
+                {
+                    if (distance < distanceRange)
+                    {
+                        foreach (string city in cityDistances[cityName][distance])
+                        {
+                            coveredCities.Add(city);
+                        }
+                    }
+                }
+            }
+            return coveredCities;
+        }
+
+        public void GetNearestAndFarthestCities()
+        {
+            int nearest = Int32.MaxValue;
+            string[] nearestCities = new string[2];
+            int farthest = Int32.MinValue;
+            string[] farthestCities = new string[2];
+            foreach (object[] s in dictionary)
+            {
+                int distance = (int)s[2];
+                if (distance == 0)
+                {
+                    continue;
+                }
+                if (distance > farthest)
+                {
+                    farthest = distance;
+                    farthestCities = new string[] { (string)s[0], (string)s[1] };
+                }
+                if (distance < nearest)
+                {
+                    nearest = distance;
+                    nearestCities = new string[] { (string)s[0], (string)s[1] };
+                }
+            }
+            Console.WriteLine("Nearest cities: " + nearestCities[0] + ", " + nearestCities[1] + ". Distance between these two cities: " + nearest);
+            Console.WriteLine("Farthest cities: " + farthestCities[0] + ", " + farthestCities[1] + ". Distance between these two cities: " + farthest);
+        }
+
+        public ArrayList MostVisitedCities(string startCity, int maxDistance)
+        {
+            List<ArrayList> nextCities = new List<ArrayList>();
+            nextCities.Add(new ArrayList { startCity, 0, new List<string>(), new HashSet<string> { startCity } });
+
+            ArrayList answer = new ArrayList { int.MaxValue, new List<string>(), 0 };
+
+            while (nextCities.Count > 0)
+            {
+                ArrayList currentCity = nextCities[0];
+                nextCities.RemoveAt(0);
+
+                string city = (string)currentCity[0];
+                int distance = (int)currentCity[1];
+                List<string> path = (List<string>)currentCity[2];
+                HashSet<string> visitedSet = (HashSet<string>)currentCity[3];
+
+                if (path.Count > ((List<string>)answer[1]).Count)
+                {
+                    answer = new ArrayList { distance, new List<string>(path), path.Count };
+                }
+
+                foreach (string targetCity in cities)
+                {
+                    HashSet<string> visited = new HashSet<string>(visitedSet);
+
+                    if (visited.Contains(targetCity))
+                    {
+                        continue;
+                    }
+
+                    int newDistance = distance + cityToCityDistances[city][targetCity];
+
+                    if (newDistance > maxDistance)
+                    {
+                        continue;
+                    }
+
+                    visited.Add(targetCity);
+                    List<string> newPath = new List<string>(path) { targetCity };
+
+                    nextCities.Add(new ArrayList { targetCity, newDistance, newPath, visited });
+                }
+            }
+
+            return new ArrayList { answer[0], new List<string>(((List<string>)answer[1])), answer[2] };
+        }
+
+        public void RandomCityDistances()
+        {
+            Random random = new Random();
+            int[] randomCities = new int[5];
+            string randomCityNamesStr = "";
+            for (int i = 0; i < 5; i++)
+            {
+                randomCities[i] = random.Next(0, 81);
+                randomCityNamesStr += cities[randomCities[i]] + ", ";
+            }
+            int[][] randomCityDistances = new int[5][];
+            for (int i = 0; i < 5; i++)
+            {
+                randomCityDistances[i] = new int[5];
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    randomCityDistances[i][j] = distances[randomCities[i]][randomCities[j]];
+                }
+            }
+
+            Console.WriteLine("Random Cities: " + randomCityNamesStr);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(randomCityDistances[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:/Users/bekir/Desktop/ilmesafe.txt";
+            string[] lines = File.ReadAllLines(filePath); 
+            string[][] matrix = new string[lines.Length][];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] lineValues = lines[i].Split('\t'); 
+                matrix[i] = lineValues; 
+            }
+            string[] cities = new string[81];
+            int[][] distances = new int[81][];
+            ArrayList dictionary = new ArrayList();
+            Dictionary<string, Dictionary<int, List<string>>> cityDistances = new Dictionary<string, Dictionary<int, List<string>>>();
+            Dictionary<string, Dictionary<string, int>> cityToCityDistances = new Dictionary<string, Dictionary<string, int>>();
+            Cities allCities = new Cities(cities, distances, dictionary, cityDistances, cityToCityDistances);
+
+            allCities.ProcessMatrix(matrix);
+
+            List<string> coveredCities = allCities.GetCoveredCities();
+            Console.WriteLine("Total number of cities: " + coveredCities.Count);
+            Console.Write("Covered cities: ");
+            foreach (string city in coveredCities)
+            {
+                Console.Write($"{city}, ");
+            }
+            Console.WriteLine("\n");
+
+            allCities.GetNearestAndFarthestCities();
+
+            Console.Write("\nEnter a city name: ");
+            string startingCity = Console.ReadLine();
+            Console.Write("Enter the maximum distance you can travel: ");
+            int maxDistance = int.Parse(Console.ReadLine());
+            ArrayList mostVisitedCitiesResult = allCities.MostVisitedCities(startingCity, maxDistance);
+
+            Console.WriteLine($"Most visited cities: {string.Join(" , ", (List<string>)mostVisitedCitiesResult[1])}");
+            Console.WriteLine($"Total distance: {(int)mostVisitedCitiesResult[0]} km");
+            Console.WriteLine($"Total number of cities: {(int)mostVisitedCitiesResult[2]}");
+
+            Console.WriteLine("\nRandom 5 city distances matrix:");
+            allCities.RandomCityDistances();
+
+            Console.WriteLine();
+            Console.WriteLine("Jagged array:");
+            Console.WriteLine();
+
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            StreamReader dosya = new StreamReader(desktop + "\\iller.txt");//iller.txt dosyası masaüstünde kalsın
+            string[] iller = {"",
+        "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya",
+        "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir",
+        "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur",
+        "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli",
+        "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan",
+        "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane",
+        "Hakkari", "Hatay", "Iğdır", "Isparta", "İstanbul",
+        "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars",
+        "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir",
+        "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya",
+        "Manisa", "Mardin", "Mersin", "Muğla", "Muş",
+        "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize",
+        "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas",
+        "Şanlıurfa", "Şırnak", "Tekirdağ", "Tokat", "Trabzon",
+        "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak",""};
+
+            // Jagged Array oluşturmak için örnek
+            int[][] jaggedArray = null;
+
+            // İlk boyutu belirleme
+            jaggedArray = new int[81][];
+
+
+            for (int i = 0; i < iller.Length - 2; i++)
+            {
+                jaggedArray[i] = new int[i + 1];
+                string satir = dosya.ReadLine();
+                string[] inputs = satir.Split('	'); //DOSYADAKI VERİLERİ AYIRMAK İÇİN
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    jaggedArray[i][j] = Convert.ToInt32(inputs[j]);
+                }
+            }
+            // Jagged Array'i yazdırma
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+
+                Console.Write(iller[i + 1] + " ");
+                for (int j = 0; j < jaggedArray[i].Length - 1; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    
+}
---
+++ PerceptronModel.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Neuron
+{
+    private double[] weights;
+    private double bias;
+    private double learningRate;
+
+    // Constructor
+    public Neuron(int inputSize, double learningRate)
+    {
+        // Başlangıçta ağırlıkları [0, 1] arasında rastgele pozitif double değerlerle başlatır
+        Random random = new Random();
+        weights = new double[inputSize];
+        for (int i = 0; i < inputSize; i++)
+        {
+            weights[i] = random.NextDouble();
+        }
+
+        // Bias'i de [0, 1] arasında rastgele pozitif double değerle başlatır
+        bias = random.NextDouble();
+
+        // Öğrenme katsayısını ayarlar
+        this.learningRate = learningRate;
+    }
+
+    // Çıktıyı hesaplar
+    public double CalculateOutput(double[] inputs)
+    {
+        // Girişlerle ağırlıkların iç çarpımını alır ve bias ekleyerek toplar
+        double sum = bias;
+        for (int i = 0; i < weights.Length; i++)
+        {
+            sum += weights[i] * inputs[i];
+        }
+
+        // Sigmoid aktivasyon fonksiyonunu uygular
+        return Sigmoid(sum);
+    }
+
+    // Ağırlıkları günceller
+    public void UpdateWeights(double[] inputs, double target)
+    {
+        double output = CalculateOutput(inputs);
+        double error = target - output;
+
+        // Ağırlıkları günceller
+        for (int i = 0; i < weights.Length; i++)
+        {
+            weights[i] += learningRate * error * inputs[i];
+        }
+
+        // Bias'i günceller
+        bias += learningRate * error;
+    }
+
+    // Sigmoid aktivasyon fonksiyonu
+    private double Sigmoid(double x)
+    {
+        return 1.0 / (1.0 + Math.Exp(-x));
+    }
+
+    // Yeni veriler üzerinde tahmin yapar
+    public void PredictAndPrint(List<double[]> newTestData)
+    {
+        Console.WriteLine("\nYeni Veriler ve Tahmin Sonuçları:");
+        foreach (var entry in newTestData)
+        {
+            double[] inputs = new double[] { entry[0], entry[1] };
+            double target = entry[2];
+
+            double output = CalculateOutput(inputs);
+
+            Console.WriteLine($"Giriş: {entry[0]}, {entry[1]} | Hedef Çıktı: {target} | Tahmin Çıktı: {output}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Eğitim verileri
+        List<double[]> trainingData = new List<double[]>
+        {
+            new double[] { 7.6 / 10, 11.0 / 15, 77.0 / 100 }, 
+            new double[] { 8.0 / 10, 10.0 / 15, 70.0 / 100 },
+            new double[] { 6.6 / 10, 8.0 / 15, 55.0 / 100 },
+            new double[] { 8.4 /10,10.0 / 15,78.0 / 100 },
+            new double[] { 8.80 / 10 ,12.0 / 15,95.0 / 100 },
+            new double[] { 7.2 / 10,10.0 / 15,67.0 / 100 },
+            new double[] { 8.1 / 10,11.0 / 15 ,80.0 / 100},
+            new double[] { 9.5 / 10,9.0 / 15,87.0 / 100},
+            new double[] { 7.3 / 10,9.0 / 15,60.0 / 100},
+            new double[] { 8.9 / 10,11.0 / 15,88.0 / 100},
+            new double[] { 7.5 / 10,11.0 / 15, 72.0 / 100},
+            new double[] { 7.6 / 10, 9.0 / 15, 58.0 / 100},
+            new double[] { 7.9 / 10,10.0 / 15,70.0 / 100 },
+            new double[] { 8.0 / 10,10.0 / 15,76.0 / 100 },
+            new double[] { 7.2 / 10,9.0 / 15,58.0 / 100 },
+            new double[] { 8.8 / 10,10.0 / 15,81.0 / 100 },
+            new double[] { 7.6 / 10, 11.0 / 15, 74.0 / 100 },
+            new double[] { 7.5 / 10,10.0 / 15,67.0 / 100 },
+            new double[] { 9.0 / 10,10.0 / 15,82.0 / 100 },
+            new double[] { 7.7 / 10,9.0 / 15,62.0 / 100 },
+            new double[] { 8.1 / 10,11.0 / 15,82.0 / 100 },
+
+           
+        };
+
+        // Yeni veriler
+        List<double[]> newTestData = new List<double[]>
+        {
+            new double[] { 8.2 / 10, 12.0 / 15, 0 },  // Sınav Sonucu belirtilmemiş, 0 olarak geçici olarak atandı
+            new double[] { 6.5 / 10, 9.0 / 15, 0 },
+            new double[] { 9.0 / 10, 13.0 / 15, 0 },
+            new double[] { 7.0 / 10, 10.0 / 15, 0 },
+            new double[] { 8.5 / 10, 11.0 / 15, 0 }
+        };
+
+        // Giriş boyutu
+        int inputSize = 2; // Çalışma Süresi ve Derse Devam
+
+        // Eğitim sonrası tahminler için kullanılacak parametreler
+        int[] epochsList = { 10, 50, 100 };
+        double[] learningRatesList = { 0.01, 0.025, 0.05 };
+
+        // Eğitim sonrası tahminler ve MSE değerlerini hesaplar ve yazdırır
+        CalculateAndPrintMSEValues(trainingData, newTestData, inputSize, epochsList, learningRatesList);
+    }
+
+    // Eğitim sonrası tahminler ve MSE değerlerini hesaplar ve yazdırır
+    static void CalculateAndPrintMSEValues(List<double[]> trainingData, List<double[]> newTestData, int inputSize, int[] epochsList, double[] learningRatesList)
+    {
+        Console.WriteLine("Eğitim Sonrası Tahminler ve MSE Değerleri:");
+
+        foreach (var epochs in epochsList)
+        {
+            foreach (var learningRate in learningRatesList)
+            {
+                // Yapay sinir hücresi oluşturur
+                Neuron neuron = new Neuron(inputSize, learningRate);
+                // Eğitim işlemi
+                for (int epoch = 0; epoch < epochs; epoch++)
+                {
+                    foreach (var entry in trainingData)
+                    {
+                        double[] inputs = new double[] { entry[0], entry[1] };
+                        double target = entry[2];
+
+                        // Ağı eğit ve ağırlıkları günceller
+                        neuron.UpdateWeights(inputs, target);
+                    }
+                }
+
+                // Eğitim sonrası tahminler
+                Console.WriteLine($"\nEpochs: {epochs}, Learning Rate: {learningRate}");
+                foreach (var entry in trainingData)
+                {
+                    double[] inputs = new double[] { entry[0], entry[1] };
+                    double target = entry[2];
+
+                    double output = neuron.CalculateOutput(inputs);
+
+                    Console.WriteLine($"Giriş: {entry[0]}, {entry[1]} | Hedef Çıktı: {target} | Tahmin Çıktı: {output}");
+                }
+
+                // MSE Hesaplama
+                double mse = CalculateMeanSquareError(neuron, trainingData);
+
+                Console.WriteLine($"MSE (Mean Square Error): {mse}");
+
+                // Yeni veriler üzerinde tahmin yapar
+                neuron.PredictAndPrint(newTestData);
+            }
+        }
+    }
+
+    // MSE Hesaplama
+    static double CalculateMeanSquareError(Neuron neuron, List<double[]> data)
+    {
+        double sumSquaredErrors = 0;
+
+        foreach (var entry in data)
+        {
+            double[] inputs = new double[] { entry[0], entry[1] };
+            double target = entry[2];
+
+            double output = neuron.CalculateOutput(inputs);
+
+            double error = target - output;
+            sumSquaredErrors += error * error;
+        }
+
+        return sumSquaredErrors / data.Count;
+    }
+}
